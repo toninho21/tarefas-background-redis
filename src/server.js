eff1741c854ce724d6c2f155d3a08fcf3d85a32c
toninho21 <toninho21@uol.com.br>
2021-01-24 11:55:17 -0300

    Alteracao em src/server.js colocar observacao

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import UserController from './app/controller/UserController';
 import BullBoard from "bull-board";
 import Queue from './app/lib/Queue';
 
-//BullBoard.setQueues(Queue.queues.map(queue => queues.bull));
+BullBoard.setQueues(Queue.queues.map(queue => queues.bull));
 
 const app = express();
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 app.post('/users', UserController.store);
 
 //Para o Bull-board
-//app.use('/admin/queues', BullBoard.UI);
+app.use('/admin/queues', BullBoard.UI);
 
 app.listen(8080, () => {
     console.log(`Servidor rodando na porta = ${process.env.PORT}`);
